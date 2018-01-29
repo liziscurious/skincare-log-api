a6a05d8b45cbbd12a59247968a7385850d2f9bef
liziscurious <32523128+liziscurious@users.noreply.github.com>
2018-01-28 18:26:51 -0800

    Can now delete a log when it has entries

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ApplicationRecord
-  has_many :entries
+  has_many :entries, dependent: :destroy
   has_many :products, through: :entries
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_01_27_185832) do
+ActiveRecord::Schema.define(version: 2018_01_28_185447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
